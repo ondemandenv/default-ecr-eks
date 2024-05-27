3ed2f66def03c1ab1fdf903f3bfca2d687766b62
jj <jj@ondemandenv.dev>
2024-05-27 15:23:43 -0400

    2st try

--- lib/default-ecr-eks-stack.ts
+++ lib/default-ecr-eks-stack.ts
@@ -1,8 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {
-    ContractsCrossRefConsumer, ContractsEnverCdk, OndemandContracts,
-    AnyContractsEnVer, ContractsEnverCdkDefaultEcrEks, EksManifest
+    ContractsEnverCdk, ContractsEnverCdkDefaultEcrEks, EksManifest
 } from "@ondemandenv/odmd-contracts";
 import {Deployment, Ingress, Service} from "cdk8s-plus-28";
 import {App, Chart} from "cdk8s";
@@ -14,33 +13,23 @@ export class DefaultEcrEksStack extends cdk.Stack {
         const revStr = m.targetRevision.type == 'b' ? m.targetRevision.value : m.targetRevision.toString();
         super(scope, ContractsEnverCdk.SANITIZE_STACK_NAME(`${m.owner.buildId}--${revStr}`), props);
 
-        let csm: ContractsCrossRefConsumer<AnyContractsEnVer, AnyContractsEnVer>
-        for (const e of OndemandContracts.inst.odmdBuilds) {
-            const aa = e.node.findAll().find(n => m.k8s.deployment.containers![0].image.endsWith(n.node.path))
-            console.log(aa)
-            if (aa) {
-                csm = aa as ContractsCrossRefConsumer<AnyContractsEnVer, AnyContractsEnVer>
-                break;
-            }
+        const chart = new Chart(new App(), 'theChart')
+
+        new Deployment(chart, 'deploy', m.simpleK8s.deployment)
+
+        if (m.simpleK8s.ingress) {
+            new Ingress(chart, 'ingress', m.simpleK8s.ingress)
         }
-
-        const chart = new Chart(new App(), 'tmp')
-
-        new Deployment(chart, 'deploy', m.k8s.deployment)
-
-        if (m.k8s.ingress) {
-            new Ingress(chart, 'ingress', m.k8s.ingress)
-        }
-        if (m.k8s.service) {
-            new Service(chart, 'service', m.k8s.service)
+        if (m.simpleK8s.service) {
+            new Service(chart, 'service', m.simpleK8s.service)
         }
 
 
         new EksManifest(this, 'd-i-s', {
             manifest: chart,
             enver: m,
-            k8sNamespace: m.k8s.targetNamespace,
-            targetEksCluster: m.k8s.targetEksCluster
+            k8sNamespace: m.simpleK8s.targetNamespace,
+            targetEksCluster: m.simpleK8s.targetEksCluster
         });
 
     }
