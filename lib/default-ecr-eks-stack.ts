82f301f261e72bafde0c108c72d7cbbc95244a3f
jj <jj@ondemandenv.dev>
2024-06-03 14:27:19 -0400

    tsting !

--- lib/default-ecr-eks-stack.ts
+++ lib/default-ecr-eks-stack.ts
@@ -1,10 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
+import {RemovalPolicy} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {
-    ContractsEnverCdk, ContractsEnverCdkDefaultEcrEks, EksManifest
-} from "@ondemandenv/odmd-contracts";
+import {ContractsEnverCdk, ContractsEnverCdkDefaultEcrEks, EksManifest} from "@ondemandenv/odmd-contracts";
 import {Deployment, Ingress, Service} from "cdk8s-plus-28";
 import {App, Chart} from "cdk8s";
+import {Bucket} from "aws-cdk-lib/aws-s3";
 
 
 export class DefaultEcrEksStack extends cdk.Stack {
@@ -14,6 +14,10 @@ export class DefaultEcrEksStack extends cdk.Stack {
         super(scope, ContractsEnverCdk.SANITIZE_STACK_NAME(`${m.owner.buildId}--${revStr}`), props);
 
         const chart = new Chart(new App(), 'theChart')
+
+        // @ts-ignore
+        m.simpleK8s.deployment.containers![0]!.envVariables = {}
+        m.simpleK8s.deployment.containers![0]!.envVariables!['tmp-bucket-tst'] = {value: new Bucket(this, 'tmp-tst', {removalPolicy: RemovalPolicy.DESTROY}).bucketName}
 
         new Deployment(chart, 'deploy', m.simpleK8s.deployment)
 
